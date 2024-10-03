f61aba7a5f46961aa41848924bd835dcda909ba5
H1rono <hronok66@gmail.com>
2024-10-03 09:26:43 +0900

    :recycle: Use `repository` package in `./cmd/connect-db`

--- cmd/connect-db/main.go
+++ cmd/connect-db/main.go
@@ -2,72 +2,28 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/H1rono/entdemo/ent"
+	repo "github.com/H1rono/entdemo/repository"
 
 	_ "github.com/lib/pq"
 )
 
-type DbConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Db       string
-}
-
-func DbConfigFromEnv() *DbConfig {
-	host, ok := os.LookupEnv("POSTGRES_HOST")
-	if !ok {
-		host = "localhost"
-	}
-	port, ok := os.LookupEnv("POSTGRES_PORT")
-	if !ok {
-		port = "5432"
-	}
-	user, ok := os.LookupEnv("POSTGRES_USER")
-	if !ok {
-		return nil
-	}
-	password, ok := os.LookupEnv("POSTGRES_PASSWORD")
-	if !ok {
-		return nil
-	}
-	db, ok := os.LookupEnv("POSTGRES_DB")
-	if !ok {
-		return nil
-	}
-	return &DbConfig{
-		Host:     host,
-		Port:     port,
-		User:     user,
-		Password: password,
-		Db:       db,
-	}
-}
-
-func (c *DbConfig) DSN() string {
-	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", c.Host, c.Port, c.User, c.Db, c.Password)
-}
-
 func main() {
-	dbConfig := DbConfigFromEnv()
+	dbConfig := repo.ConfigFromEnv()
 	if dbConfig == nil {
 		log.Fatal("Missing required environment variables")
 	}
-	dbClient, err := ent.Open("postgres", dbConfig.DSN())
+	repository, err := repo.Connect(dbConfig)
 	if err != nil {
-		log.Fatalf("failed opening connection to postgres: %v", err)
+		log.Fatalf("failed connecting to database: %v", err)
 	}
 	defer func() {
-		if err := dbClient.Close(); err != nil {
-			log.Fatalf("failed closing connection to postgres: %v", err)
+		if err := repository.Close(); err != nil {
+			log.Fatalf("failed closing connection: %v", err)
 		}
 	}()
-	if err := dbClient.Schema.Create(context.Background()); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
+	if err := repository.Migrate(context.Background()); err != nil {
+		log.Fatalf("failed migration: %v", err)
 	}
 }
