67c7bf5d625e1d7b47597ee1f4941aa9f96f0188
H1rono <hronok66@gmail.com>
2024-10-03 09:35:22 +0900

    :sparkles: Use repository in `router.Router`

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"log"
+
+	"github.com/H1rono/entdemo/repository"
 	"github.com/H1rono/entdemo/router"
 	"github.com/labstack/echo/v4"
-	"log"
 )
 
 func main() {
+	dbConfig := repository.ConfigFromEnv()
+	if dbConfig == nil {
+		log.Fatal("Missing required environment variables")
+	}
+	repo, err := repository.Connect(dbConfig)
+	if err != nil {
+		log.Fatalf("failed connecting to database: %v", err)
+	}
 	e := echo.New()
-	r := router.New()
+	r := router.New(repo)
 	r.SetupRoutes(e)
 	log.Fatal(e.Start(":1323"))
 }
--- router/router.go
+++ router/router.go
@@ -1,11 +1,16 @@
 package router
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/H1rono/entdemo/repository"
+	"github.com/labstack/echo/v4"
+)
 
-type Router struct{}
+type Router struct {
+	r *repository.Repository
+}
 
-func New() *Router {
-	return &Router{}
+func New(repo *repository.Repository) *Router {
+	return &Router{repo}
 }
 
 func (r *Router) SetupRoutes(e *echo.Echo) {
